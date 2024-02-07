5d49b046dcd2590011d3e166d3b110b88d3e1dd5
Colette Ulianogit config --global --listclear <culiano@live.com>
2024-02-07 13:40:03 -0500

    ThemeProvider in place for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import type { Component } from 'solid-js';
+import { ThemeProvider } from 'solid-theme-provider';
 
 import logo from './logo.svg';
 import styles from './App.module.css';
@@ -7,6 +8,7 @@ const App: Component = () => {
   return (
     <div class={styles.App}>
       <header class={styles.header}>
+        <ThemeProvider />
         <img src={logo} class={styles.logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
